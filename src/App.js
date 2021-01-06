c8b154719973e2d68857dd06c5aab5841a5fff52
Your Name <you@example.com>
2021-01-06 17:11:59 -0300

    comecei a desenvolver o filtro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Box = styled.div`
+margin: 1vw;
+padding-left: 1vw;
+padding-right: 1vw;
+border: 1px solid black;
+display: flex;
+flex-direction:column;
+height: 97vh;
+width: 22vw;
+`
+
+const Input = styled.input`
+display: flex;
+margin-bottom: 1vh;
+width: 75%
+`
+
+class Filtro extends React.Component{
+  render(){
+    return(
+      <Box>
+      
+      <h2>Filtro:</h2>
+      <label for="VMin" > Valor Mínimo: </label>
+      <input id="VMin" type = "number" onChange=""/>
+
+      <label for="VMax"> Valor Máximo: </label>
+      <input id="VMax" type = "number" onChange=""/>
+
+      <label for="BuscaProd"> Buscar Produto: </label>
+      <input id="BuscaProd" type = "text" onChange=""/> 
+      
+      </Box>
+    )
+  }
 }
 
-export default App;
+
+export default class App extends React.Component{
+  render (){
+    return (
+      <div>
+      
+      <Filtro />
+
+      </div>
+    )
+  }
+}
+
