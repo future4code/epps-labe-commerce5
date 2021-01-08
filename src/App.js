852bf8a5400d5c74f51bc3cb6f1be2cb72db52bf
WBernardo <souzawermeson@gmail.com>
2021-01-08 16:43:49 -0300

    Estilização e Filtro de Busca

--- src/App.js
+++ src/App.js
@@ -3,74 +3,88 @@ import styled from 'styled-components'
 import Carrinho from './Components/Carrinho/Carrinho';
 import Filtro from './Components/Filtro/Filtro';
 import Home from './Components/Home/Home'
-import QuantidadeProdutos from './Components/Home/QuantidadeProdutos';
+import Header from './Components/Home/Header';
 
 const Loja = styled.div`
-display: flex;
-flex-direction: column;
-border: 1px solid black; 
-margin: 1vw;
-height: 99vh;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+background-color: #1C1C1C;
 width: 100vw;
 `
 const ContainerProdutos = styled.div `
 display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+grid-template-rows: 1fr 1fr;
+text-align: center;
+color: white;
 `
 
 export default class App extends Component {
-  produtos = [{
+  state= {
+  produtos: [{
     id: 1,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Esqui em Urano",
+    value: 51000,
+    imageUrl: "https://cdn.pixabay.com/photo/2012/01/09/10/56/uranus-11625_960_720.jpg",
   },
   {
     id: 2,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Campos Brilhantes Lunares",
+    value: 36000,
+    imageUrl: "https://cdn.pixabay.com/photo/2011/12/13/16/24/moon-11026_960_720.jpg",
   },
   {
     id: 3,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Núpcias em Vênus",
+    value: 25000,
+    imageUrl: "https://cdn.pixabay.com/photo/2011/12/13/14/39/venus-11022_960_720.jpg",
   },
   {
     id: 4,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Monte Olímpo de Marte",
+    value: 20000,
+    imageUrl: "https://cdn.pixabay.com/photo/2011/12/13/14/30/mars-11012_960_720.jpg",
   },
   {
     id: 5,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Manchas de Júpiter",
+    value: 32000,
+    imageUrl: "https://cdn.pixabay.com/photo/2012/10/26/02/44/moon-63136_960_720.jpg",
   },
   {
     id: 6,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Anéis de Saturno",
+    value: 43000,
+    imageUrl: "https://cdn.pixabay.com/photo/2012/11/28/10/54/saturn-67671_960_720.jpg",
   },
   {
     id: 7,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Bate-Volta em Mercúrio",
+    value: 57000,
+    imageUrl: "https://cdn.pixabay.com/photo/2012/01/09/09/33/mercury-11591_960_720.png",
   },
   {
     id: 8,
-    name: "Foguete da Missão Apollo 11",
-    value: 10000.0,
-    imageUrl: "https://picsum.photos/200/200",
+    name: "Kitesurf em Netuno",
+    value: 74000,
+    imageUrl: "https://cdn.pixabay.com/photo/2012/11/28/09/17/neptune-67537_960_720.jpg",
   },
-  ]
+  ],
 
+  textoInput: ''
+}
+onChangeTextoInput = (e) =>{
+  this.setState({textoInput:e.target.value})
+  }
 
+filtraProdutosBusca = () => {
+  return this.state.produtos.filter(produto => produto.name.includes(this.state.textoInput) ) 
+}
+  
   render() {
-    const componentProduto = this.produtos.map((produto) => {
+    const listaFiltrada = this.filtraProdutosBusca()
+
+    const componentProduto = listaFiltrada.map((produto) => {
       return (
         <div>
           <Home
@@ -82,17 +96,16 @@ export default class App extends Component {
       )
     }) 
     return(
-      <Loja>
-        <Filtro />
-
-        <div>
-          <QuantidadeProdutos />
+      <div>
+        <Header lista={this.state.produtos} />
+      <Loja >
+        <Filtro filtraProdutosBusca={this.onChangeTextoInput} textoInput={this.state.textoInput}/>        
+        <ContainerProdutos>
         {componentProduto}
-        </div>
-
+        </ContainerProdutos>        
         <Carrinho />
       </Loja>
+      </div>
     )
   }
 }
-
--- src/Components/Carrinho/Carrinho.js
+++ src/Components/Carrinho/Carrinho.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const Cart = styled.div`
-border: 1px solid black;
 margin-top: 1vw;
+margin-left: 1.5vw;
 padding-left: 1vw;
 padding-right: 1vw;
+border: 1px solid white;
 height: 95vh;
-width: 13vw;
-position: relative;
+width: 15vw;
+color: white;
 `
-
 export default class Carrinho extends Component {
     render() {
         return (
             <Cart>
-                <h3>Carrinho</h3>
+                <h2>Carrinho</h2>
                 <p>1x Produto <button>X</button></p>
                 <p>1x Produto<button>X</button></p>
                 <p>Total: R$ 396.00</p>
